be8edf8268d362612778d71e217daf567d1cb098
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2021-09-03 13:38:40 +0900

    環境構築

---
+++ Assets/src/Utility/Checker.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//指定矩形範囲内にCheckableがあるかどうかをチェックする
+//もしあれば、そのCheckableのイベントを実行させる
+//親にRigitbody2Dがあるとうまく動かないときがある。なぜ？
+public class Checker : MonoBehaviour
+{
+    /// <summary>
+    /// この範囲にCheckableがあったらチェックできる
+    /// </summary>
+    [SerializeField] Collider2D m_collider = default;
+
+    /// <summary>
+    /// 現在のCheckable
+    /// 複数あったら最後に範囲に入ったオブジェクトに上書きされる
+    /// </summary>
+    Checkable currentCheckable = null;
+
+    //public string targetTag;
+
+    /// <summary>
+    /// エリア内にCheckableがあるか
+    /// </summary>
+    /// <param name="player"></param>
+    /// <returns>true : Checkableがある false : ない</returns>
+    public bool HasCheckable() { return  currentCheckable != null; }
+
+    /// <summary>
+    /// 範囲内にあるCheckableを１つチェックする
+    /// NPCであれば会話が始まったり、機械だったらメニューが開いたり
+    /// </summary>
+    /// <returns></returns>
+    public bool Check()
+    {
+        if (currentCheckable != null)
+        {
+            currentCheckable.TakeCheck(this);
+            return true;
+        }
+        return false;
+    }
+
+    int GetCollider2Ds(Vector2 position, Collider2D[] results)
+    {
+        return m_collider.OverlapCollider(new ContactFilter2D(), results);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (currentCheckable != null) return;
+        Checkable checkable;
+        if (collision.TryGetComponent<Checkable>(out checkable))
+        {
+            currentCheckable = checkable;
+            currentCheckable.OnEnter.Invoke(this);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (currentCheckable == null) return;
+        if (collision.TryGetComponent<Checkable>(out _))
+        {
+            currentCheckable = null;
+            currentCheckable.OnExit.Invoke(this);
+        }
+    }
+
+    /*public void SetAngle(float angleDeg)
+    {
+        transform.rotation = Quaternion.Euler(0, 0, angleDeg + 90f);
+    }*/
+}
---
+++ Assets/src/Utility/Scene/SceneChangeWrapper.cs
@@ -0,0 +1,107 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Kyoichi
+{
+    public class Context
+    {
+        public Context() { }
+        public Context(Dictionary<string, object> dat)
+        {
+            data = dat;
+        }
+        public Dictionary<string, object> data = new Dictionary<string, object>();
+    }
+
+    public class SceneChangeWrapper : SingletonMonoBehaviour<SceneChangeWrapper>
+    {
+
+        public enum StackMode
+        {
+            AddToTop,
+            SwapTop,
+        }
+
+        [SerializeField] GameObject loadingUI;
+        [SerializeField] string fadeInAnimationName;
+        [SerializeField] string fadeOutAnimationName;
+
+        Stack<KeyValuePair<string, Context>> stack = new Stack<KeyValuePair<string, Context>>();
+
+        GameObject uiInstance;
+        Animator uiAnimator;
+
+
+        private void Start()
+        {
+            uiInstance = Instantiate(loadingUI);
+            uiAnimator = uiInstance.GetComponent<Animator>();
+            DontDestroyOnLoad(this);
+            DontDestroyOnLoad(uiInstance);
+            uiInstance.SetActive(false);
+            stack.Push(new KeyValuePair<string, Context>(SceneManager.GetActiveScene().name, null));
+        }
+
+        public async UniTask SceneChangeAsync(string targetScene, Context context, float fade = 1f, StackMode mode = StackMode.AddToTop)
+        {
+            // シーン切り替え
+            uiInstance.SetActive(true);
+            uiAnimator.Play(fadeInAnimationName);
+            //アニメーションが再生しきったら
+            await UniTask.WaitUntil(() => uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime == 1);
+            if (mode == StackMode.AddToTop)
+            {
+                stack.Push(new KeyValuePair<string, Context>(targetScene, context));
+            }
+            else
+            {
+                stack.Pop();
+                stack.Push(new KeyValuePair<string, Context>(targetScene, context));
+            }
+           await PlaySceneAsync(targetScene, context);
+            uiAnimator.Play(fadeOutAnimationName);
+            //アニメーションが再生しきったら
+            await UniTask.WaitUntil(() => uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime == 1);
+            uiInstance.SetActive(false);
+        }
+
+        public void Back(Context context = null, float fade = 1f)
+        {
+            stack.Pop();
+            var top = stack.Peek();
+            if (context == null)
+            {
+                PlaySceneAsync(top.Key, top.Value);
+            }
+            else
+            {
+                PlaySceneAsync(top.Key, context);
+            }
+        }
+
+        public void Restart(Context context = null, float fade = 1f)
+        {
+            var top = stack.Peek();
+            if (context == null)
+            {
+                PlaySceneAsync(top.Key, top.Value);
+            }
+            else
+            {
+                PlaySceneAsync(top.Key, context);
+            }
+        }
+
+      async  UniTask PlaySceneAsync(string sceneName, Context context)
+        {
+            SceneManager.LoadScene(sceneName);
+            await FindObjectOfType<SceneFunctioner>().SceneStartAsync(context);
+
+        }
+
+    }
+}
